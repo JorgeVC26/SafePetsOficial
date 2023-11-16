25e5967e8853a1927f00312cb3df58ef3925c9b2
JorgeVC26 <jorvillacar26@gmail.com>
2023-11-16 16:02:33 -0600

    Confirmación eliminar usuarios

--- src/Components/Usuarios/Usuarios.js
+++ src/Components/Usuarios/Usuarios.js
@@ -1,41 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import '../../css/cards.css';
+import '../../css/cards.css'; // Asegúrate de importar tus estilos CSS
 import { useNavigate } from 'react-router-dom';
 
 function Usuarios() {
-  const [users, setUsers] = useState([]); // Inicializa como un arreglo vacío
+  const [users, setUsers] = useState([]);
+  const [confirmDelete, setConfirmDelete] = useState(false);
+  const [userToDeleteId, setUserToDeleteId] = useState(null);
+  const [userDeletedMessage, setUserDeletedMessage] = useState('');
   const navigate = useNavigate();
 
   const usuarioActivo = JSON.parse(localStorage.getItem('UsuarioActivo'));
   const rol = usuarioActivo ? usuarioActivo[0].role : null;
 
-  // Verifica si el usuario tiene el rol necesario para ver a los usuarios
-  const canViewUsers = rol === "superadmin" || rol === "admin";
+  const canViewUsers = rol === 'superadmin' || rol === 'admin';
 
   useEffect(() => {
-    // Obtén los datos de usuarios del Local Storage al cargar el componente
     const storedUsers = JSON.parse(localStorage.getItem('users'));
     if (storedUsers && Array.isArray(storedUsers)) {
       setUsers(storedUsers);
     }
   }, []);
 
+  const confirmDeleteHandler = () => {
+    const updatedUsers = users.filter((user) => user.id !== userToDeleteId);
+    setUsers(updatedUsers);
+    localStorage.setItem('users', JSON.stringify(updatedUsers));
+    setConfirmDelete(false);
+    setUserToDeleteId(null);
+    setUserDeletedMessage('Usuario eliminado con éxito');
+    // Limpia el mensaje después de unos segundos (ajusta el tiempo según tus necesidades)
+    setTimeout(() => setUserDeletedMessage(''), 3000);
+  };
+
+  const cancelDeleteHandler = () => {
+    setConfirmDelete(false);
+    setUserToDeleteId(null);
+  };
+
   const handleDelete = (id) => {
-    if (rol === "superadmin") {
-      // Solo el superadmin puede eliminar usuarios
-      const updatedUsers = users.filter((user) => user.id !== id);
-      setUsers(updatedUsers);
-      localStorage.setItem('users', JSON.stringify(updatedUsers));
+    if (rol === 'superadmin') {
+      setConfirmDelete(true);
+      setUserToDeleteId(id);
     } else {
       alert('No tienes permisos para eliminar usuarios.');
     }
   };
 
   const handleAddUser = () => {
-    
-    if (rol === "admin" || rol === "superadmin") {
-      // Solo el admin y el superadmin pueden agregar usuarios
+    if (rol === 'admin' || rol === 'superadmin') {
       navigate('/CrearUsuario');
     } else {
       alert('No tienes permisos para agregar usuarios.');
@@ -43,8 +56,7 @@ function Usuarios() {
   };
 
   const handleEditUser = (id) => {
-    if (rol === "superadmin" || rol === "admin") {
-      // Solo el admin y el superadmin pueden editar usuarios
+    if (rol === 'superadmin' || rol === 'admin') {
       navigate(`/editarUsuario/${id}`);
     } else {
       alert('No tienes permisos para editar usuarios.');
@@ -52,20 +64,46 @@ function Usuarios() {
   };
 
   return canViewUsers ? (
-    <div className='contenedor-usuarios'>
-            <header>
+    <div className="contenedor-usuarios">
+      <header>
         <div>
           <Link to="/" style={{ textDecoration: 'none' }}>
-          <a className="titulo" href="/">
-            <h1>Safe<span>Pets</span></h1>
-          </a>
+            <a className="titulo" href="/">
+              <h1>
+                Safe<span>Pets</span>
+              </h1>
+            </a>
           </Link>
         </div>
-
       </header>
       <h2>Lista de Usuarios</h2>
-      <button className="green-button" onClick={handleAddUser}>Agregar Usuario</button>
-      <Link className="back-button"  to="/">Regresar</Link>
+      <button className="green-button" onClick={handleAddUser}>
+        Agregar Usuario
+      </button>
+      <Link className="back-button" to="/">
+        Regresar
+      </Link>
+
+      {confirmDelete && (
+        <div className="confirmation-modal">
+          <h3>¿Estás seguro de que quieres eliminar este usuario?</h3>
+          <div className="confirmation-modal-buttons">
+            <button className="confirm" onClick={confirmDeleteHandler}>
+              Confirmar
+            </button>
+            <button className="cancel" onClick={cancelDeleteHandler}>
+              Cancelar
+            </button>
+          </div>
+        </div>
+      )}
+
+      {userDeletedMessage && (
+        <div className="user-deleted-message">
+          <p>{userDeletedMessage}</p>
+        </div>
+      )}
+
       <table className="styled-table">
         <thead>
           <tr>
@@ -81,19 +119,27 @@ function Usuarios() {
               <td>{user.id}</td>
               <td>{user.name}</td>
               <td>{user.email}</td>
-   
-              <td className='actions'>
-                <button className="blue-button" onClick={() => handleEditUser(user.id)}>Editar</button>
-                <button className="red-button" onClick={() => handleDelete(user.id)}>Eliminar</button>
-                </td>
-
+              <td className="actions">
+                <button
+                  className="blue-button"
+                  onClick={() => handleEditUser(user.id)}
+                >
+                  Editar
+                </button>
+                <button
+                  className="red-button"
+                  onClick={() => handleDelete(user.id)}
+                >
+                  Eliminar
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
       </table>
     </div>
   ) : (
-    <div className='container'>
+    <div className="container">
       <h2>No tiene permisos</h2>
     </div>
   );
